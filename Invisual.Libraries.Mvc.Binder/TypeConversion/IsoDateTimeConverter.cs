6ef28268d2a4454d03cb2b06eee8bcc94d242840
Tom Bowers <tom@newmind.co.uk>
2017-07-08 14:25:56 +1000

    Added IsoDateTimeConverter.

---
+++ Invisual.Libraries.Mvc.Binder/TypeConversion/IsoDateTimeConverter.cs
@@ -0,0 +1,13 @@
+﻿using Invisual.Libraries.Mvc.ModelBinding.TypeConversion;
+using System;
+
+namespace Invisual.Libraries.Mvc.Binder.TypeConversion
+{
+  public class IsoDateTimeConverter : ITypeConverter<DateTime>
+  {
+    public DateTime Convert(string input)
+    {
+      return DateTime.Parse(input);
+    }
+  }
+}
